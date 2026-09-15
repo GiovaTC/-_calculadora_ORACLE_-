e43ea72e65d26059041f5d5f29c8f39971705722
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-09-15 11:20:09 -0500

    commit - Operacion / .

--- Models/Operacion.cs
+++ Models/Operacion.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace CalculadoraOracle_19C.Models
 {
-    internal class Operacion
+    public class Operacion
     {
-    }
+        public int Id { get; set; }
+        public decimal Numero1 { get; set; }
+        public string Operador { get; set; } = string.Empty;
+        public decimal Numero2 { get; set; }
+        public decimal Resultado { get; set; }
+        public DateTime FechaOperacion { get; set; }
+    }   
 }
